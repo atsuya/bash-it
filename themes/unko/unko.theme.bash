c1458546b34fab9a9266298181f9ead2bba3cfae
Atsuya Takagi <atsuya.takagi@gmail.com>
2011-09-16 01:11:14 -0700

    added my theme.

---
+++ themes/unko/unko.theme.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCM_THEME_PROMPT_PREFIX=" ${reset_color}[${green}"
+SCM_THEME_PROMPT_SUFFIX="${reset_color}]"
+SCM_THEME_PROMPT_DIRTY=" ${yellow}⚡"
+SCM_THEME_PROMPT_CLEAN=" "
+
+prompt() {
+  PS1="${blue}\W${reset_color}$(scm_prompt_info)${reset_color} \$ "
+}
+
+PROMPT_COMMAND=prompt
