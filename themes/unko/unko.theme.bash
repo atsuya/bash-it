160a2a8da639aa066d77766b025e636c0db0f66c
Atsuya Takagi <atsuya.takagi@gmail.com>
2011-09-16 01:15:21 -0700

    updated,

--- themes/unko/unko.theme.bash
+++ themes/unko/unko.theme.bash
@@ -3,7 +3,7 @@
 SCM_THEME_PROMPT_PREFIX=" ${reset_color}(${green}"
 SCM_THEME_PROMPT_SUFFIX="${reset_color})"
 SCM_THEME_PROMPT_DIRTY=" ${yellow}⚡"
-SCM_THEME_PROMPT_CLEAN=" "
+SCM_THEME_PROMPT_CLEAN=""
 
 prompt() {
   PS1="${blue}\W${reset_color}$(scm_prompt_info)${reset_color} \$ "
