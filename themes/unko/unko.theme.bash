72d1bb7ed5527a35ea582043b1743885b6c77699
Atsuya Takagi <atakagi@optimiscorp.com>
2012-03-27 15:11:36 -0700

    added prompt indication for stash.

--- themes/unko/unko.theme.bash
+++ themes/unko/unko.theme.bash
@@ -5,8 +5,12 @@ SCM_THEME_PROMPT_SUFFIX="${reset_color})"
 SCM_THEME_PROMPT_DIRTY=" ${yellow}⚡"
 SCM_THEME_PROMPT_CLEAN=""
 
+function parse_git_stash {
+  [[ $(git stash list 2> /dev/null | tail -n1) != "" ]] && echo " ${red}☃"
+}
+
 prompt() {
-  PS1="${blue}\W${reset_color}$(scm_prompt_info)${reset_color} \$ "
+  PS1="${blue}\W${reset_color}$(scm_prompt_info)$(parse_git_stash)${reset_color} \$ "
 }
 
 PROMPT_COMMAND=prompt
